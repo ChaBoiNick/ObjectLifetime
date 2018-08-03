09fa2709e0654f967313f36f39c7f9c258cb0398
Nicholas Schmidt <talkschmidt@hotmail.com>
2018-08-03 14:20:55 +1000

    Set up

--- ObjectLifetime/Class1.cs
+++ ObjectLifetime/Class1.cs
@@ -2,7 +2,25 @@
 
 namespace ObjectLifetime
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+
     }
 }
